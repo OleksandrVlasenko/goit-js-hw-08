0cd2c6f5a156bb3ebf990fc8fff46a19f929feff
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-03-14 23:43:50 +0200

    Tasks 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const markupForRendering = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+				      <img class="gallery__image" src="${preview}" alt="${description}"/>
+			      </a>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markupForRendering);
+
+// Прибираємо нижні падінги у img
+const imgEl = document.querySelectorAll('.gallery img');
+
+imgEl.forEach(elem => (elem.style.display = 'block'));
+
+// Створюємо галерею Simplelightbox
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(
+    Number(localStorage.getItem('videoplayer-current-time'))
+  );
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', String(data.seconds));
+  }, 1000)
+);
+
+player.on('ended', function () {
+  setTimeout(() => {
+    localStorage.removeItem('videoplayer-current-time');
+  }, 2000);
+});
