11efdcc1b683b4e5ea0e328656c01f732848b3fc
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-03-15 02:33:17 +0200

    Task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,6 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
 const galleryEl = document.querySelector('.gallery');
 
 // Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const feedbackFormState = {};
+const FEEDBACK_KEY = 'feedback-form-state';
+const savedMessage = load(FEEDBACK_KEY);
+
+if (savedMessage && savedMessage.email) {
+  form.elements.email.value = savedMessage.email;
+  feedbackFormState.email = savedMessage.email;
+}
+
+if (savedMessage && savedMessage.message) {
+  form.elements.message.value = savedMessage.message;
+  feedbackFormState.message = savedMessage.message;
+}
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    feedbackFormState[event.target.name] = event.target.value;
+    save(FEEDBACK_KEY, feedbackFormState);
+  }, 500)
+);
+
+form.addEventListener('submit', sendMessage);
+
+function sendMessage(event) {
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    alert('Всі поля мають бути заповнені');
+    return;
+  }
+  event.preventDefault();
+  console.log(feedbackFormState);
+  feedbackFormState.email = '';
+  feedbackFormState.message = '';
+  remove(FEEDBACK_KEY);
+  form.reset();
+}
+
+function save(key, value) {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
+
+function load(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+function remove(key) {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Remove state error: ', error.message);
+  }
+}
